96f51e53b085cca685910414916aed48d2faa900
KONG Zijun <35690049+zijunkong@users.noreply.github.com>
2024-04-24 10:38:23 +0200

    Add files via upload

---
+++ exo1.py
@@ -0,0 +1,59 @@
+# My Python Code Forever
+
+
+'''
+My
+Python
+Code
+every
+day
+'''
+
+
+int_type = 3
+float_type = 3.12
+string_type = "variable"
+dict_type = {1:"1",
+             2:"2"}
+llist_type = [1,2,3,4,5,6,7,8,9]
+set_type = (1,2,3,4,5,6,7,8,9)
+
+my42count = "quarante-deux"
+print(len(my42count))
+
+
+variable_42 = 42 if not variable_condition
+
+
+myArray42 = list("quarante-deux")
+print(myArray42)
+
+
+myArray42Len = len(myArray42)
+print("Le nombre d'index dans myArray42 est :", myArray42Len)
+
+
+recomposed_word = "".join(myArray42)
+final_string = recomposed_word + " La grande réponse sur la vie, l’univers et le reste !"
+
+rand = 9
+verification = rand == 42
+print(verification)
+
+
+My42Type = 3
+print(type(My42Type))
+
+
+compute42 = 2 * 21
+compute42 = str(compute42)
+
+ex12 = 42424242
+ex12 = str(ex12)
+ex12 = ex12.replace("42","quarante-deux ")
+
+a = 24
+b = 42
+a,b = b,a
+
+
