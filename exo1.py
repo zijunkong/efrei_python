c8ab19bca679b4d356561513178f5e3b74f73541
KONG Zijun <35690049+zijunkong@users.noreply.github.com>
2024-04-24 11:07:27 +0200

    Update exo1.py

--- exo1.py
+++ exo1.py
@@ -16,13 +16,16 @@ string_type = "variable"
 dict_type = {1:"1",
              2:"2"}
 llist_type = [1,2,3,4,5,6,7,8,9]
-set_type = (1,2,3,4,5,6,7,8,9)
+tuple_type = (1,2,3,4,5,6,7,8,9)
 
 my42count = "quarante-deux"
 print(len(my42count))
 
 
-variable_42 = 42 if not variable_condition
+variable_exist = 10 
+variable_detection = locals().get('variable_existe', 42)
+print(variable_detection)
+
 
 
 myArray42 = list("quarante-deux")
@@ -42,6 +45,12 @@ print(verification)
 
 
 My42Type = 3
+My42Type = 3.12
+My42Type = "variable"
+My42Type = {1:"1",
+             2:"2"}
+My42Type = [1,2,3,4,5,6,7,8,9]
+My42Type = (1,2,3,4,5,6,7,8,9)
 print(type(My42Type))
 
 
